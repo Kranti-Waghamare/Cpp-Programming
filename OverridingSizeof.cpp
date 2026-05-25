1a1584f186a46ae22ffb8dde9ac9d2ecde654941
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-05-25 11:52:34 +0530

    Size of overriding in cpp

---
+++ OverridingSizeof.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int i,j;
+
+        void fun()
+        {cout<<"Inside Base fun\n"; }
+
+        void gun()
+        {cout<<"Inside Base gun\n"; }
+
+        void sun()
+        {cout<<"Inside Base sun\n"; }
+
+
+};   //8 Bytes
+
+class Derived : public Base
+{
+    public:
+        int x,y;
+
+        void fun()                     //Redefinition
+        {cout<<"Inside Derived fun\n";  }
+
+        void sun()                     //Redefinition
+        {cout<<"Inside Derived sun\n";  }
+
+        void run()                     //Definition
+        {cout<<"Inside Derived run\n";  }
+
+
+
+};   //16 Bytes
+
+int main()
+{
+    cout<<sizeof(Base)<<"\n";        //8
+    cout<<sizeof(Derived)<<"\n";     //16
+
+    return 0;
+}
